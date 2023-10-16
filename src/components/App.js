7bf9893c90833af56b4e1d6be5f2fdee97ef5c14
surnaturel <numirix.yannick@gmail.com>
2023-10-16 19:02:53 +0000

    Premier commit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import Banner from './Banner'
+import Cart from './Cart'
+import ShoppingList from './ShoppingList'
+function App() {
+    return (<div>
+        <Banner />
+        <Cart />
+        <ShoppingList />
+        </div>)
+}
+
+export default App
