f91936ece2ed751955295ae289291d8208d0e0f1
surnaturel <numirix.yannick@gmail.com>
2024-02-09 19:19:47 +0000

    je doit finir

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,21 @@
 import Banner from './Banner'
 import logo from '../assets/logo.png'
 // import Cart from './Cart'
+import Footer from './Footer'
 import ShoppingList from './ShoppingList'
-import QuestionForm from './QuestionForm'
 
 function App() {
-	function handleSubmit(e) {
-		e.preventDefault()
-		alert(e.target['my_input'].value)
-	}
 	return (
 		<div>
 			<Banner>
 				<img src={logo} alt='La maison jungle' className='lmj-logo' />
 				<h1 className='lmj-title'>La maison jungle</h1>
 			</Banner>
+
 			{/* <Cart /> */}
 			<ShoppingList />
-			<form onSubmit={handleSubmit}>
-				<input type='text' name='my_input' defaultValue='Tapez votre texte' />
-				<button type='submit'>Entrer</button>
-			</form>
-			<QuestionForm />
+			<Footer />
 		</div>
-		
 	)
 }
 
